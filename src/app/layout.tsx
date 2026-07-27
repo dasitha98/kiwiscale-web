d08c60a3e4860cad8c1e8abe59a9d6bd82293485
dasitha98 <dasitha98@gmail.com>
2026-07-27 19:40:42 +1200

    themecolor changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
